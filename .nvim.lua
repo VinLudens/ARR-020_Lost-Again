96f71d9a786f8c6a298944a3a6811511d7f95028
niveK77pur <niveK77pur@users.noreply.github.com>
2023-12-30 21:18:38 +0000

    Apply automatic changes

--- .nvim.lua
+++
@@ -1,9 +0,0 @@
-require("nvls").setup({
-    lilypond = {
-        options = {
-            main_file = "Lost-Again.ly",
-            include_dir = "./openlilylib",
-        },
-    },
-})
-vim.g.disable_autoformat = true
